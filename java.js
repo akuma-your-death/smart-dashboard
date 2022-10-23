53bc2e8c6cd0af2ba8c85de4c5e8455ddb92498a
akuma <116360575+akuma-your-death@users.noreply.github.com>
2022-10-23 18:18:54 +0200

    Delete java.js

--- java.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function(){
-    window.location.reload(1);
- }, 10000);
- var datetime = new Date();
- console.log(datetime);
- document.getElementById("time").textContent = datetime; //it will print on html page 
