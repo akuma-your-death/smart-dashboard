f09a39579da03f95e76df17789745ba23316997e
akuma <116360575+akuma-your-death@users.noreply.github.com>
2022-10-24 19:10:39 +0200

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,138 @@
+import os
+import socket
+import webbrowser
+import http.server
+import socketserver
+import time
+import webbrowser
+import platform
+import os
+os.system('cls||clear')
+
+with open("nmap_target_answer.txt", "w") as file:
+    file.write("set first a target in dashboard.py")
+
+with open("target.txt", "w") as file:
+    file.write("0")
+
+sys = platform.system()
+if sys[:3] == "Win":
+    os.system("start /min cmd /k python server.py")
+# Webserver Port
+port = 8000
+
+# IPv4 for webserver 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = s.getsockname()[0]
+
+with open("ip-client.txt", "w") as file:
+    file.write(ip)
+
+ip_for_proof = ip[:12]
+
+routers_website = input(str("whats your standart gateway?(its your ip but the last number*s is a 1 if you dont know your IP write ip)(XXX.XXX.XXX.1) "))
+while routers_website != "ip" or routers_website[:12] != ip_for_proof:
+    with open("routers_ip.txt", "w") as file:
+        file.write(f"{routers_website}")
+    with open("routers_ip.txt", "r") as file:
+        routers_website = file.read()
+    if routers_website == "ip":
+        print(ip)
+    if routers_website != "ip" and routers_website[:12] != ip_for_proof:
+        print("thats not a correct mode write (ip or your standard gateway)")
+    routers_website = input("whats your standard gateway? ")
+    if routers_website[:12] == ip_for_proof:
+        with open("routers_ip.txt", "w") as file:
+            file.write(routers_website)
+        break
+if routers_website[:12] != ip_for_proof:
+    not_right_ip = False
+while routers_website[:2] == "ip":
+    with open("routers_ip.txt", "w") as file:
+        file.write(f"{routers_website}")
+    with open("routers_ip.txt", "r") as file:
+        routers_website = file.read()
+    print(f"your IP: {ip} ")
+    routers_website = input("whats your standard gateway? ")
+    
+os.system('cls||clear')
+if routers_website == "":
+    with open("routers_ip.txt", "r") as file:
+        routers_website = file.read()
+# Sets Routers IP the same as in the input 
+with open('routers_ip.txt','r') as file:
+    routers_website = file.read()
+if sys[:3] == "Win":
+    os.system("""
+start /min cmd /k python nmap.py
+""")
+if sys[:3] == "Lin":
+    os.system('xterm -3 python3 nmap.py')
+print("pleas wait for like 1 minute while creating file and starting nmap(you can watch one of my german Videos on youtube while that https://www.youtube.com/watch?v=ANXkX6ixaxg)")
+
+
+# Creates index.html if it doesnt exist and writes html code in it 
+with open("index.html", "w") as file:
+    file.write(f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="refresh" content="30" />
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3 style="color: white; text-align: center;">The time is: <span id="time" style="color: white;"> </span></h3>
+        <h4 style="color: white; text-align: center;">your wifi is <span id="wifi_speed"> </span>MB's fast (but on a local file thats why its more then normal)</h4>
+        <button class="glow-on-hover" type="button" onclick="window.location.href ='http://192.168.178.1'">
+            <span style="color: white;">
+                log to your Router in
+            </span>
+        </button>
+    <p>
+        <button class="glow-on-hover" type="button" onclick="window.location.href ='network_ips.txt'">
+            <span style="color: white;">
+                <p>
+                    show all ips from network
+                </p>
+            </span>
+        </button>
+        <p>
+            <button class="glow-on-hover" type="button" onclick="window.location.href ='nmap_target_answer.txt'">                
+                <span style="color: white;">
+                    <p>
+                        get Target's nmap report (write nmap in dashoard.py)
+                    </p>
+                </span>            
+            </button>
+        </p>
+        
+</body>
+<script src="java.js">
+
+</script>
+</html>
+""")
+
+while True:
+    try:
+        with open("network_ips.txt", "r") as file:
+            network_ips = file.read()
+    except FileNotFoundError as file_not_found:
+        pass
+    mode = input("choose mode ")
+    if mode == "nmap":
+        nmap_target = input("target ip ")
+        with open("target.txt", "r") as file:
+            target_True = file.read()
+        if target_True == "0" and nmap_target != "":
+            with open("target2.txt", "w") as file:
+                file.write("1")
+        with open("target.txt", "w") as file:
+            file.write("1")
+        with open("nmap_target_ip.txt", "w") as file:
+            file.write(f"{nmap_target}")
